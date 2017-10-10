c5d4a6eb3c9cf11f79cf784440e0250c7f4f5022
github username <github email address>
2017-10-10 15:58:13 -0400

    All done with the project!

--- bin/run.rb
+++ bin/run.rb
@@ -4,5 +4,10 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-character = get_character_from_user
+character=nil
+results=get_character_movies_from_api(character)
+until results.include?(character)
+  character=get_character_from_user
+end
+
 show_character_movies(character)
