1dba15d36532f1e599106202de3f5edb4e7f550c
MatteBru <mbruni616@gmail.com>
2017-10-10 17:12:43 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,33 +1,29 @@
 require 'rest-client'
 require 'json'
-require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+def rubyize(url)
+  info = JSON.parse(RestClient.get(url))
 end
 
-def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def get_character_movies_from_api(character)
+  film_urls = []
+  character_hash = rubyize("http://www.swapi.co/api/people/")
+  character_hash["results"].each do |char|
+    film_urls = char["films"] if char["name"].downcase == character
+  end
+  film_urls.map{|url| rubyize(url)}
+end
+
+def parse_character_movies(film_list)
+  film_list.each do |film|
+    puts "Episode #{film["episode_id"]}: #{film["title"]}"
+    puts "Released: #{film["release_date"]}"
+    puts "Directed by: #{film["director"]}"
+    puts "Produced by: #{film["producer"]}\n\n"
+  end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
