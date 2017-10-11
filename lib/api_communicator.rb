f2be4a8b042bbcefbf72864b6f006dd98d06c167
Abe Rudy <aberudy@gmail.com>
2017-10-10 22:05:53 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,12 +2,12 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+=begin
 def get_character_movies_from_api(character) #working
   #make the web request
   films = []
   films_hash = []
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
+  character_hash = get_characters_hash
   character_hash["results"].each do |char| #an array, with information about each character
     if char["name"].downcase == character.downcase #if the element matches the character argument
       films = char["films"] #store the films #films, an array of links to their films
@@ -16,34 +16,66 @@ def get_character_movies_from_api(character) #working
 films_hash = films.map { |link| RestClient.get(link) } 
 films_hash = films_hash.map {|element| JSON.parse(element)} 
 films_hash
+=end
+
+
+def get_character_movies_from_api(character) #working, less code, more function
+  char_hash = get_characters_hash
+  char = char_hash.find {|char| char["name"].downcase == character.downcase}
+    films = char["films"]
+  films_hash = films.map { |link| RestClient.get(link) } 
+films_hash = films_hash.map {|element| JSON.parse(element)} 
+films_hash
 end
 
-def parse_character_movies(films_hash)   #helper method to parse the data in :films to extract information                                    #and print it in a nice list
+
+
+def parse_character_movies(films_hash) #working                                   
   films_hash.select.with_index do |film, index|
     puts "#{index+1}. Episode #{film["episode_id"]}: #{film["title"]}"
     puts "Release Date: #{film["release_date"]}"
-    puts "Description: #{film["opening_crawl"]}"
   end
 end
 
-def show_character_movies(character) 
+def get_movies_info(film_name) #bonus
+  films_hash = get_character_movies_from_api
+      if film["title"] == requested_film
+        current_film = film
+      end 
+  film
+end
+
+def show_character_movies(character) #working
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
 
+def get_character_data(character) #bonus
+  films_hash = get_character_movies_from_api(character)
+  get_movies_info(films_hash)
+end
 
 
+def get_characters_hash #working - add results key to hash, directly pull results 
+  characters = RestClient.get('http://www.swapi.co/api/people/')
+  char_hash = JSON.parse(characters)["results"]
+  char_hash
+end
+
+def get_movie_list #bonus_main_component
+  films = []
+  film_hash = []
+  char_hash = get_characters_hash
+  char_hash.collect do |person|
+    films << person["films"]
+  end
+  films = films.uniq
+end
+
+
+
+  
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
-
- # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -7,3 +7,5 @@ def get_character_from_user
   input = gets.chomp# use gets to capture the user's input. This method should return that input, downcased.
   input.downcase
 end
+
+
